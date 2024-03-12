9d931eb3287e69fc2a3bb3a9ae1059ae0db6aaac
Nitin Kumar <199nitinkumar@gmail.com>
2024-03-12 12:45:40 +0530

    loader

--- 06reactRouter/src/components/GitHub/Github.jsx
+++ 06reactRouter/src/components/GitHub/Github.jsx
@@ -1,15 +1,17 @@
-import { useEffect, useState } from "react";
+// import { useEffect, useState } from "react";
+import { useLoaderData } from "react-router-dom";
 
 function Github() {
-  const [data, setData] = useState([]);
-  useEffect(() => {
-    fetch("https://api.github.com/users/nitin-kumar199")
-      .then((response) => response.json())
-      .then((data) => {
-        console.log(data);
-        setData(data);
-      });
-  }, []);
+  const data = useLoaderData();
+  //   const [data, setData] = useState([]);
+  //   useEffect(() => {
+  //     fetch("https://api.github.com/users/nitin-kumar199")
+  //       .then((response) => response.json())
+  //       .then((data) => {
+  //         console.log(data);
+  //         setData(data);
+  //       });
+  //   }, []);
   return (
     <div className="text-center m-4 bg-gray-600 text-white p-4 text-3xl">
       Github Followers: {data.followers}
@@ -19,3 +21,7 @@ function Github() {
 }
 
 export default Github;
+export const githubInfoLoader = async () => {
+  const response = await fetch("https://api.github.com/users/nitin-kumar199");
+  return response.json();
+};
