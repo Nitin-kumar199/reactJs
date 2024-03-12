07a860a9dd63a9208ce056205702b890eb326099
Nitin Kumar <199nitinkumar@gmail.com>
2024-03-12 12:41:31 +0530

    router

---
+++ 06reactRouter/src/components/GitHub/Github.jsx
@@ -0,0 +1,21 @@
+import { useEffect, useState } from "react";
+
+function Github() {
+  const [data, setData] = useState([]);
+  useEffect(() => {
+    fetch("https://api.github.com/users/nitin-kumar199")
+      .then((response) => response.json())
+      .then((data) => {
+        console.log(data);
+        setData(data);
+      });
+  }, []);
+  return (
+    <div className="text-center m-4 bg-gray-600 text-white p-4 text-3xl">
+      Github Followers: {data.followers}
+      <img src={data.avatar_url} height={300} width={300} />
+    </div>
+  );
+}
+
+export default Github;
